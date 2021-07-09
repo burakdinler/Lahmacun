e01d0c1799ec738ccfdd01ac9033489d8f011dd1
Burak Dinler <burak@autom8labs.ai>
2021-07-09 15:50:13 +0300

    change 22

--- OperationsLibrary/GitOperations.cs
+++ OperationsLibrary/GitOperations.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,10 +13,24 @@ namespace OperationsLibrary
     {
         public void GetDiff()
         {
-            Process.Start("git", "init");
-            Process.Start("git","remote add origin https://github.com/burakdinler/Lahmacun.git");
-            Process.Start("git", "fetch");
-            Process.Start("git", "archive --output=archived_changes.zip HEAD $(git diff --diff-filter=ACMRTUXB)");
+            //Process.Start("git", "init");
+            //Process.Start("git","remote add origin https://github.com/burakdinler/Lahmacun.git");
+            //Process.Start("git", "fetch");
+            //Process.Start("git", "archive --output=archived_changes.zip HEAD $(git diff --diff-filter=ACMRTUXB --name-only 95a1550a d4fea7b8)");
+
+
+            string rootPath = Repository.Init("D:\\Deneme\\");
+            //var repo = new Repository("D:\\Deneme\\");
+            //repo.Network.Remotes.Add("origin", "https://github.com/burakdinler/Lahmacun.git");
+            string logMessage = "";
+            using (var repo = new Repository(rootPath))
+            {
+                var remote = repo.Network.Remotes["origin"];
+                var refSpecs = remote.FetchRefSpecs.Select(x => x.Specification);
+                Commands.Fetch(repo, remote.Name, refSpecs, null, logMessage);
+            }
+
+            Console.WriteLine(logMessage);
         }
     }
 }
