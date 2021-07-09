1c1d720fa177bdc9075e31dded29cb52e0c3fb5f
Burak Dinler <burak@autom8labs.ai>
2021-07-09 14:47:59 +0300

    Add project files.

---
+++ OperationsLibrary/GitOperations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibGit2Sharp;
+
+namespace OperationsLibrary
+{
+    public class GitOperations
+    {
+        public void GetDiff()
+        {
+            Process.Start("git init");
+            Process.Start("git remote add origin https://github.com/burakdinler/Lahmacun.git");
+            Process.Start("git archive -o update.zip HEAD $(git diff --name-only HEAD^)");
+
+        }
+    }
+}
